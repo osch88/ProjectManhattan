618fecc90726fbec46356fbd34a878a2459b5ab9
osch88 <kristiansson.oscar@gmail.com>
2021-11-15 13:27:35 +0100

    init
    Change-Id: I358f4c5098a5730434a3da57b5faaaeb252660b9

--- app/reo/main.cpp
+++ app/reo/main.cpp
@@ -1,10 +1,25 @@
 #include <iostream>
+#include "../include/engine.hpp"
 
 
+int main(int argc, char **argv) {
 
-int main(int argc, char *argv[]){
+    std::cout << "ENGINE EMULATOR" << std::endl;
 
+    Engine engine;
+
+    engine.setThrottle(50);
+    engine.setEngineStatus(true);
+    engine.runEngine();
     
+    if(engine.getEngineStatus()) {
+        std::cout << "Engine is on" << std::endl;
+    } else {
+        std::cout << "Engine is NOT on" << std::endl;
+    }
+
+    std::cout << "RPM: "<< engine.getRPM() << std::endl;
+
 
     return 0;
 }
