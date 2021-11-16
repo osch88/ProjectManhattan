175682ce8f9d91ee7883d6abb9c5a0fbe3aa5aa5
Johan Granholm <johan.m.granholm@gmail.com>
2021-11-16 15:36:52 +0100

    keyboard input added
    Change-Id: Ib664e8f8c42e4ea22ef18294e28744f02bf20d8c

---
+++ app/inputhandler/keyboard_input.cpp
@@ -0,0 +1,112 @@
+#include <iostream>
+#include <string>
+#include <unistd.h>
+
+/* - keyboard inputs are: 
+    CHR=DEC
+    S=115        for start
+    Q=113        for stop
+    P=112        for gear position Park
+    N=110        for gear position Neutral
+    D=100        for gear position Drive
+    R=114        for gear position Reverse
+    UP=65       for gas pedal increase (acceleration)
+    DOWN=66     for gas pedal decrease (deccelration)
+    1=49        for drive mode normal (default if non selected)
+    2=50        for drive mode sport 
+    K=107
+    L=108
+    */
+
+void inputText(){
+    std::cout << "Keyboard inputs: " << std::endl;
+    std::cout << "\rS: start \n \rQ: stop \n\rP: gear position in park \n\rN: gear position neutral \n"
+        << "\rD: gear position in drive \n\rR: gear position in reverse \n\rUP-arrow: accelerate \n"
+        << "\rDOWN-arrow: decelerate \n\r1: drive mode normal \n\r2: drive mode sport \n\r. : gracefully shutdown" << std::endl;
+}
+    //too long function, how to break up?
+void inputHandler(){
+    //sending initial state()
+    
+    char input;
+    int int_input;
+    int gasPedalPos = 0;
+    //noecho()
+
+    // Set the terminal to raw mode
+    while(1) {
+        inputText();
+        system("stty raw");
+        input = getchar(); 
+        int_input = input;
+        std::cout << input << std::endl;
+        //sleep(1);
+        
+        // terminate when "." is pressed
+        system("stty cooked");
+        system("clear");
+        system("");
+        switch (int_input){
+        case 115:
+            std::cout << "Start button was pressed" << std::endl;
+            //call function()
+            break;
+        case 113:
+            std::cout << "Stop button was pressed" << std::endl;
+            break;
+        case 112:
+            std::cout << "Gear position in park" << std::endl;
+            break;
+        case 110:
+            std::cout << "Gear position in neutral" << std::endl;
+            break;
+        case 100:
+            std::cout << "Gear position in drive" << std::endl;
+            break;
+        case 114:
+            std::cout << "Gear position in reverse" << std::endl;
+            break;
+        case 108:
+            gasPedalPos += 10;
+            if (gasPedalPos > 100){
+                gasPedalPos = 100;
+            }
+            std::cout << "Accelerate: " << gasPedalPos << "%" << std::endl;
+            break;
+        case 107:
+            gasPedalPos -= 10;
+            if (gasPedalPos < 0){
+                gasPedalPos = 0;
+            }
+            std::cout << "Decelerate: " << gasPedalPos << "%" << std::endl;
+            break;
+        case 49:
+            std::cout << "Drive mode COMFORT selected" << std::endl;
+            break;
+        case 50:
+            std::cout << "Drive mode DYNAMIC selected" << std::endl;
+            break;
+        case 46:
+            //std::cout << "Gracefully shutting down in: 3 seconds" << std::endl;
+            for(auto i=3; i>0; i--){
+                std::cout << "Gracefully shutting down in: " << i << " seconds" << std::endl;
+                sleep(1);
+                system("clear");
+            }
+            system("stty cooked");
+            exit(0);
+            break;
+        default:
+            std::cout << "Please press a valid button, see manual for input" << std::endl;
+            break;
+        }
+    }
+}
+
+int main() {
+
+    inputHandler();
+
+    return 0;
+    
+}
