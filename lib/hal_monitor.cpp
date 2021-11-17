f2516a6c4a1100a6a2872cad1039aef76151a22f
Christopher Bengtsson <christopher.bengtsson@volvocars.com>
2021-11-17 14:00:14 +0100

    added hal_monitor
    Change-Id: Id7938652bdc17668a52930b5988c3f9815b47967

---
+++ lib/hal_monitor.cpp
@@ -0,0 +1,78 @@
+#include "hal_monitor.hpp"
+#include <iostream>
+
+HalMonitor::HalMonitor() {
+    latest_received_data_.drive_mode = database_type::DriveMode::kEco;
+    latest_received_data_.gas_pedal = 0;
+    latest_received_data_.gear = database_type::Gear::kPark;
+    latest_received_data_.ignition = database_type::Ignition::kStop;
+}
+
+bool HalMonitor::ReadFromCan(SocketCan &socket_can) {
+    bool return_value = true;
+    CanFrame frame;
+    if (socket_can.ReadFromCan(frame) == SocketCanStatus::kStatusOk) {
+        if (frame.id == can_data_base::start_button.frame_id or can_data_base::drive_mode.frame_id) {
+            UpdateDataForStartButton(frame);
+            UpdateDataForDriveMode(frame);
+        } else if (frame.id == can_data_base::pedal.frame_id) {
+            UpdateDataForPedalPosition(frame);
+        } else if (frame.id == can_data_base::gear.frame_id) {
+            UpdateDataForGearPosition(frame);
+        }
+    } else {
+        return_value = false;
+    }
+    return return_value;
+}
+
+void HalMonitor::UpdateDataForGearPosition(const CanFrame &frame) {
+    if (frame.data[0] == 1){
+        latest_received_data_.gear = database_type::Gear::kPark;
+    } else if (frame.data[0] == 2){
+        latest_received_data_.gear = database_type::Gear::kReverse;
+    } else if (frame.data[0] == 3){
+        latest_received_data_.gear = database_type::Gear::kDrive;
+    } else if (frame.data[0] == 4){
+        latest_received_data_.gear = database_type::Gear::kNeutral;
+    } else {
+        std::cout << "invalid data in Gear position can data, not within valid range. " << std::endl;
+    }
+}
+
+void HalMonitor::UpdateDataForPedalPosition(const CanFrame &frame) {
+    if (frame.data[0] >= 0 and frame.data[0] <= 100){
+        latest_received_data_.gas_pedal = frame.data[0]; //TODO: proper cast
+    } else {
+        std::cout << "invalid data in Pedal Position can data, not within valid range. " << std::endl;
+    }
+}
+
+void HalMonitor::UpdateDataForStartButton(const CanFrame &frame){
+    if (frame.data[0] == 1){
+        latest_received_data_.ignition = database_type::Ignition::kStop;
+    } else if (frame.data[0] == 2){
+        latest_received_data_.ignition = database_type::Ignition::kStart;
+    } else {
+        std::cout << "invalid data in start button can data, not within valid range. " << std::endl;
+    }
+}
+
+void HalMonitor::UpdateDataForDriveMode(const CanFrame &frame){
+    if (frame.data[1] == 1){
+        latest_received_data_.drive_mode = database_type::DriveMode::kEco;
+    } else if (frame.data[1] == 2){
+        latest_received_data_.drive_mode = database_type::DriveMode::kSport;
+    } else {
+        std::cout << "invalid data in Drive Mode can data, not within valid range. " << std::endl;
+    }
+}
+
+void HalMonitor::GetCanData(database_type::Database &data) {
+    data = latest_received_data_;
+}
+/*
+void HalMonitor::WriteEmulatorDataToCan(const SocketCan &socket_can, const database_type::Database &message) const {
+    
+}
+*/
