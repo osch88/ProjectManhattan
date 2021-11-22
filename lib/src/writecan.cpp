13a575650c4ca3a422168e2ad1273a0c5e43b243
Caroline <caroline.palm@volvocars.com>
2021-11-22 14:11:15 +0100

    update write can to handle output from emulator
    Change-Id: I298997c3e111ed314d7ac307e597ef9f49c5748e

--- lib/src/writecan.cpp
+++ lib/src/writecan.cpp
@@ -10,15 +10,15 @@ bool WriteUserInputToCan(SocketCan &socket, database_type::Database &db, const i
 
     can_data_base::StartButton cb_ignition; 
     database_type::Ignition db_ignition = db.ignition; 
-    const CanFrame ignition = ConvertToCanFrame<>(db_ignition, cb_ignition);
+    const CanFrame ignition = ConvertToCanFrame(db_ignition, cb_ignition);
 
     can_data_base::GearPosition cb_gear; 
     database_type::Gear db_gear = db.gear; 
-    const CanFrame gear = ConvertToCanFrame<>(db_gear, cb_gear);
+    const CanFrame gear = ConvertToCanFrame(db_gear, cb_gear);
 
     can_data_base::PedalPosition cb_gas; 
     unsigned int db_gas = db.gas; 
-    const CanFrame gas = ConvertToCanFrame<>(db_gas, cb_gas);
+    const CanFrame gas = ConvertToCanFrame(db_gas, cb_gas);
 
     auto write_ignition_status = socket.WriteToCan(ignition);
     std::this_thread::sleep_for(std::chrono::milliseconds(msdelay)); //delay for frame
@@ -27,38 +27,32 @@ bool WriteUserInputToCan(SocketCan &socket, database_type::Database &db, const i
     auto write_gas_status = socket.WriteToCan(gas);
     
     if (write_ignition_status != kStatusOk){ //kolla alla samtidigt och return false om någon misslyckas?
-        std::cout << "something went wrong on socket write for ignition, error code: "<< write_ignition_status << std::endl;
+        std::cout << "Something went wrong on socket write for ignition, error code: "<< write_ignition_status << std::endl;
         ret = false;
     }
     else if(write_gear_status != kStatusOk){
-        std::cout << "something went wrong on socket write for gear, error code : " << write_gear_status << std::endl;
+        std::cout << "Something went wrong on socket write for gear, error code : " << write_gear_status << std::endl;
         ret = false;
     }
     else if(write_gas_status != kStatusOk){
-        std::cout << "something went wrong on socket write for gas, error code : " << write_gas_status  << std::endl;
+        std::cout << "Something went wrong on socket write for gas, error code : " << write_gas_status  << std::endl;
         ret = false;
-    }
-    else{
-        std::cout << "Message was written to the socket for all frames"  << std::endl;
     }
     return ret; 
-
-
 }
 
-/*bool WriteCanFrameEmulator(SocketCan &socket, database_type::Database &db, const int &msdelay){
+bool WriteCanFrameEmulator(SocketCan &socket, database_type::Database &db, const int &msdelay){
     bool ret = true;
-    const CanFrame rpm = ConvertRpmToCanFrame(db);
-    std::this_thread::sleep_for(std::chrono::milliseconds(msdelay)); //delay for frame
+    can_data_base::Rpm cb_rpm;
+    unsigned int db_rpm = db.RPM;
+    const CanFrame rpm = ConvertToCanFrame(db_rpm, cb_rpm);
+    
     auto write_rpm_status = socket.WriteToCan(rpm);
-    std::this_thread::sleep_for(std::chrono::milliseconds(msdelay)); //delay for frame
-    if (write_rpm_status != kStatusOk){ //kolla alla samtidigt och return false om någon misslyckas?
-        printf("something went wrong on socket write for rpm, error code : %d \n", int32_t(write_rpm_status));
+    
+    if (write_rpm_status != kStatusOk){
+        std::cout << "Something went wrong on socket write for rpm, error code : " << write_rpm_status  << std::endl;
         ret = false;
     }
-    else{
-        printf("Message was written to the socket for all frames \n");
-    }
     return ret;
+}
 
-}*/
