741c48edd86b5df3fc08a05b697c33a72c4f7a9c
Christopher Bengtsson <christopher.bengtsson@volvocars.com>
2021-11-25 14:42:03 +0100

    updated unit test with another test
    Change-Id: Id3df21f8b751ae1a838bf4b2aff98be283816c8d

--- test/unit/test_hal_monitor.cpp
+++ test/unit/test_hal_monitor.cpp
@@ -10,10 +10,22 @@ void TestHalMonitor::TearDown() {
 }
 
 TEST_F(TestHalMonitor, testing_read) {
-    //TestHalMonitor *test_hal_monitor;
     SocketCan socket_can;
     bool ret = hal_monitor_->ReadFromCan(socket_can);
-    
-    
+    reo_type::Database data;
+    hal_monitor_->GetCanData(data);
+    EXPECT_EQ(data.drive_mode, reo_type::DriveMode::kEco);
+    EXPECT_EQ(data.gas, 0);
+    EXPECT_EQ(data.gear, reo_type::Gear::kPark);
+    EXPECT_EQ(data.ignition, reo_type::Ignition::kStop);
     EXPECT_EQ(ret, false);
+}
+
+TEST_F(TestHalMonitor, testing_get_can_data) {
+    reo_type::Database data;
+    hal_monitor_->GetCanData(data);
+    EXPECT_EQ(data.drive_mode, reo_type::DriveMode::kEco);
+    EXPECT_EQ(data.gas, 0);
+    EXPECT_EQ(data.gear, reo_type::Gear::kPark);
+    EXPECT_EQ(data.ignition, reo_type::Ignition::kStop);
 }
