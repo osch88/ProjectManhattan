2959fbbf9deb5451cbd4a318ebc2843bd89a8316
Caroline <caroline.palm@volvocars.com>
2021-11-25 17:15:38 +0100

    emulator with physics
    Change-Id: Id32194a239b03cbdff42a64ea963d37d04ef025c

--- app/reo/include/engine.hpp
+++ app/reo/include/engine.hpp
@@ -9,17 +9,29 @@ class Engine {
  private:
     reo_type::EngineStatus engine_status_ = reo_type::EngineStatus::kOff;
     reo_type::Gear gear_ = reo_type::Gear::kPark;
-    unsigned int rpm_ = 0;
+    double rpm_ = 0;
     unsigned int throttle_ = 0;
     unsigned int gear_number_ = 1;
-    //unsigned int target_rpm_;
+    //double traction_force_ = 0;
+    double speed_ = 0;
     const unsigned int MAX_RPM = 6000;
-    const unsigned int MIN_RPM = 800;
+    const unsigned int MIN_RPM = 875;
     void SetEngineStaus(const reo_type::Database &_input);
     void UpdateGear(const reo_type::Database &_input);
     void UpdateRpm(const reo_type::Database &_input);
+    void ShiftGear();
+    void CalcSpeed();
+    double CalcTractionForce();
+    double CalcRoadForce();
+    double CalcAeroForce();
     const double gear_ratio_[10] = {6, 5.25, 3.029, 1.95, 1.457, 1.221, 1, 0.809, 0.673, 4.015};
-    const std::map<unsigned int, double> coefficients_ = {{800, 3.2}, {2000, 3.2}, {2000, 2.5}, {5000, 2.5}, {5000, 1}, {6100, 0.85}};
+    const double drive_shaft_ratio_ = 2.955;
+    const unsigned int efficiency_ = 1;
+    const double wheel_radius_ = 0.3;
+    const double vehicle_mass_ = 2250;
+    const double drag_coefficient_ = 0.29;
+    const double front_area_ = 2.23;
+    const std::map<unsigned int, double> maxtorque_ = {{800, 210}, {2000, 280}, {2000, 280}, {3500, 310}, {5000, 300}, {6100, 270}};
  public:
     Engine() = default;
     void set_inpVal(const reo_type::Database &_input);
